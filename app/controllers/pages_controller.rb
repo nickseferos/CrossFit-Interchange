179c127d47f0f318e9e7f73bcb6791e2e41c2616
nickseferos <nick.seferos@gmail.com>
2015-10-18 17:45:25 -0700

    Tumblr works. I am so much happier now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,13 @@ class PagesController < ApplicationController
   def index
     require 'json'
     require 'open-uri'
-    @response = HTTParty.get("https://api.tumblr.com/v2/blog/crossfitinterchange.tumblr.com/posts/text?api_key=tWncHp5Q4yob4R9TwF88HznAUUnULGh1vXFRLZc7m3mplvM29t")
-    puts @response
+    response = JSON.parse(HTTParty.get("https://api.tumblr.com/v2/blog/crossfitinterchange.tumblr.com/posts?api_key=tWncHp5Q4yob4R9TwF88HznAUUnULGh1vXFRLZc7m3mplvM29t").body)
+    # @hash = response.as_json
+    # return response.as_json
+    # @title = response[:posts][:title]
+    @title = response['response']['posts'][0]['title']
+    @body = response['response']['posts'][0]['body']
+
 	end
 
 	def about_us
