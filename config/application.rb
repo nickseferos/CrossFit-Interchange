f5897fcba8133c957da231ae5db0b86bcc4b7e25
nickseferos <nick.seferos@gmail.com>
2016-01-23 19:31:05 -0800

    modifed application to allow ckeditor

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,10 @@ module CFI
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.exceptions_app = self.routes
-
+    
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   end
 end
