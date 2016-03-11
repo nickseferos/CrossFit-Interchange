f243bc673c95f2ed92fd9d4d75a1542a45b2397b
nickseferos <nick.seferos@gmail.com>
2016-03-11 09:59:16 -0800

    Added rails_admin, devise and cancan

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Ckeditor::Engine => '/ckeditor'
 	#Casein routes
 	namespace :casein do
