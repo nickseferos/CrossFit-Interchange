66d77fcf9ce00369c0d9908e2db18244e819c6af
nickseferos <nick.seferos@gmail.com>
2016-03-13 23:07:20 -0700

    added validation for free_trials

--- app/models/free_trial.rb
+++ app/models/free_trial.rb
@@ -1,2 +1,6 @@
 class FreeTrial < ActiveRecord::Base
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates_format_of :email, with: /@/
 end
