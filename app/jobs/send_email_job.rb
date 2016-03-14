b547feca32068b1b9685912d3c47b26583270141
nickseferos <nick.seferos@gmail.com>
2016-03-14 00:09:50 -0700

    fixed job error

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -1,7 +1,7 @@
 class SendEmailJob < ActiveJob::Base
   queue_as :default
 
-  def perform(FreeTrial)
+  def perform(freetrial)
     @free_trial = FreeTrial
     FreeTrialMailer.free_trial_email(@free_trial).deliver_later
     FreeTrialMailer.free_trial_engage_email(@free_trial_engage).deliver_later
