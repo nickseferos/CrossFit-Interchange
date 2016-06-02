a40c47d8bc918fef0c377cb5337de4128b9a5dcf
nickseferos <nick.seferos@gmail.com>
2016-06-02 16:36:52 -0400

    Assed phrasing to WCIF
    Added via a puts method, db storage pending approval from Austin

--- app/helpers/phrasing_helper.rb
+++ app/helpers/phrasing_helper.rb
@@ -1,9 +1,9 @@
 module PhrasingHelper
   # You must implement the can_edit_phrases? method.
   # Example:
-  #
+  # 
   def can_edit_phrases?
-   current_user.is_admin?
+   # current_user.is_admin?
+   warden.authenticate! scope: :admin
   end
-
 end
