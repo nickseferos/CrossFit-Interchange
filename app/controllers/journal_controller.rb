6923d8ff6388d5f428dd69f43972d1055f64c564
nickseferos <nick.seferos@gmail.com>
2016-06-26 12:29:24 -0700

    Added journal controller and index view for journal

---
+++ app/controllers/journal_controller.rb
@@ -0,0 +1,40 @@
+class JournalController < ApplicationController
+	def index
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def journal_params
+		
+	end
+
+	def find_journal
+		
+	end
+
+end
