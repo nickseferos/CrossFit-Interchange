c1e83244b060ccaa8f00a8c45b5b305c6096be8e
nickseferos <nick.seferos@gmail.com>
2016-10-02 13:58:33 -0700

    modifed free trial email to force change

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,6 +1,6 @@
 class JournalsController < ApplicationController
 	
-	# before_filter :authenticate_user!
+	before_filter :authenticate_user!
 	before_action :find_journal, only: [:show, :update, :edit, :destroy]
 
   def index
